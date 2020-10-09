80d11501bb16f650631e2f01fa553f27c3abc622
plentybugs <plentybugs@yandex.ru>
2020-10-09 20:28:23 +0300

    init project

---
+++ src/main/java/org/netcracker/project/NetcrackerProjectApplication.java
@@ -0,0 +1,13 @@
+package org.netcracker.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcrackerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetcrackerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/netcracker/project/NetcrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.netcracker.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetcrackerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
