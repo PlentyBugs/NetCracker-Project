0ba4f21870ef328b41493ef0b94874da763cdbda
Julia <dekamussi@yandex.ru>
2020-10-22 01:17:41 +0300

    Add missing files

--- src/main/java/org/netcracker/project/controllers/UserController.java
+++ src/main/java/org/netcracker/project/controllers/UserController.java
@@ -1,5 +1,26 @@
-package org.netcracker.projects.controllers;
+package org.netcracker.project.controllers;
 
+import org.netcracker.project.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
 public class UserController {
-    
+
+    static User user1 = new User(1, "Jessica", "Andreevna",
+            "Moskvina", "dekamussi@yandex.ru", "Jess", "hash");
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id){
+        return user1;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return Collections.singletonList(user1);
+    }
 }
--- src/main/java/org/netcracker/project/models/User.java
+++ src/main/java/org/netcracker/project/models/User.java
@@ -1,2 +1,36 @@
-package org.netcracker.project.models;public class User {
+package org.netcracker.project.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    Integer id;
+    String name;
+    String surname;
+    String lastname;
+    String email;
+    String login;
+    String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
