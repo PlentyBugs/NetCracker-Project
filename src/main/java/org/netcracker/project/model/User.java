6f352867f8a9c1715d6c0d3f13fcdcdbb66f7599
RollerM0bster <katerinasheludenko@gmail.com>
2020-10-23 22:41:15 +0300

    User updated

--- src/main/java/org/netcracker/project/model/User.java
+++ src/main/java/org/netcracker/project/model/User.java
@@ -1,6 +1,27 @@
 package org.netcracker.project.model;
 
-import javax.persistence.Table;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Data
 public class User /* implements UserDetails */ {
+    private String name;
+    private String surname;
+    private String secName;
+    private String email;
+    private String password;
+    private String login;
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
