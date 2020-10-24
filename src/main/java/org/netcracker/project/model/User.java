88717232be2d71b32db212027083739465ec24a9
RollerM0bster <katerinasheludenko@gmail.com>
2020-10-24 17:41:09 +0300

    Added @table, @NoargsConstructor, UserDetails. set,get methods removed for "id".

--- src/main/java/org/netcracker/project/model/User.java
+++ src/main/java/org/netcracker/project/model/User.java
@@ -1,27 +1,61 @@
 package org.netcracker.project.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Collection;
+
 @Entity
+@Table(name="usr")
 @Data
-public class User /* implements UserDetails */ {
+@NoArgsConstructor
+public class User implements UserDetails {
     private String name;
     private String surname;
     private String secName;
     private String email;
     private String password;
     private String login;
+    @Id
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
-    @Id
-    public Long getId() {
-        return id;
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
