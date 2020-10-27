3f0fd7de43599bbab01684ac90bb568c5dedc861
RollerM0bster <katerinasheludenko@gmail.com>
2020-10-27 14:07:41 +0300

    Classes updated.

--- src/main/java/org/netcracker/project/model/Competition.java
+++ src/main/java/org/netcracker/project/model/Competition.java
@@ -1,4 +1,26 @@
 package org.netcracker.project.model;
 
+import lombok.Data;
+import org.apache.tomcat.jni.Local;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class Competition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank(message = "Competition's name can't be empty!")
+    private String compName;
+    @DateTimeFormat(pattern = "dd.MM.yyyy HH:mm")
+    private LocalDateTime start;
+    @DateTimeFormat(pattern = "dd.MM.yyyy HH:mm")
+    private LocalDateTime end;
 }
