39daf6943875a6cab037ea78f8811c83111f05f3
RollerM0bster <katerinasheludenko@gmail.com>
2020-10-27 13:52:31 +0300

    Added repositories, classes updated.

---
+++ src/main/java/org/netcracker/project/repository/CompetitionRepository.java
@@ -0,0 +1,13 @@
+package org.netcracker.project.repository;
+
+import org.netcracker.project.model.Competition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+
+public interface CompetitionRepository extends JpaRepository<Competition,Long> {
+    Competition findByCompName(String compName);
+    // @Query(...)
+    Competition queryByStart(LocalDateTime start);
+}
