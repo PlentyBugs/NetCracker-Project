7b9c66efcc7f6744536e95fdd4fa5b66ecd01a5f
RollerM0bster <katerinasheludenko@gmail.com>
2020-11-01 19:28:43 +0300

    TeamService:added fields, methods. TeamRepository: added findAll().

--- src/main/java/org/netcracker/project/service/TeamService.java
+++ src/main/java/org/netcracker/project/service/TeamService.java
@@ -2,6 +2,8 @@ package org.netcracker.project.service;
 
 import org.netcracker.project.model.Team;
 import org.netcracker.project.model.User;
+import org.netcracker.project.repository.TeamRepository;
+import org.netcracker.project.util.ImageUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,16 +13,26 @@ import java.io.IOException;
 
 @Service
 public class TeamService {
+    private final TeamRepository repository;
+    private final ImageUtils imageUtils;
 
+    public TeamService(TeamRepository repository,ImageUtils imageUtils)
+    {
+        this.repository=repository;
+        this.imageUtils=imageUtils;
+    }
+    public Page<Team> getPage(Pageable pageable){return repository.findAll(pageable);}
     public Page<Team> getPage(Pageable pageable, String filter){    //need to write body
-        return getPage(pageable,filter);
+        return getPage(pageable);
     }
 
     public boolean save(Team team, Multipart logo, User user) throws IOException {  //need to write body
+
         return true;
     }
 
     public boolean update(Team team){   //body
         return true;
     }
+    private
 }
