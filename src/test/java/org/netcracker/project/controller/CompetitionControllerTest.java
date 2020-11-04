01fe0d25729667962ae709da3256b4500a32aef3
plentybugs <plentybugs@yandex.ru>
2020-11-04 22:40:36 +0300

    Начал писать тесты для CompetitionController

--- src/test/java/org/netcracker/project/controller/CompetitionControllerTest.java
+++ src/test/java/org/netcracker/project/controller/CompetitionControllerTest.java
@@ -10,7 +10,11 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,6 +35,34 @@ public class CompetitionControllerTest {
         assertNotNull(competitionController);
     }
 
+    @Test
+    public void competitionListCheck() throws Exception {
+        mockMvc.perform(get("/competition"))
+                .andExpect(authenticated())
+                .andExpect(xpath("//div[@id='competition-list']/div/div/a").nodeCount(2));
+    }
 
+    @Test
+    public void specificCompetitionInListTest() throws Exception {
+        mockMvc.perform(get("/competition"))
+                .andExpect(authenticated())
+                .andExpect(xpath("//div[@id='competition-list']/div[1]/div/a/img").exists())
+                .andExpect(xpath("//div[@id='competition-list']/div[1]/div/div/h5").string(containsString("Big Data Analysis MegaHackathon Moscow 2021")))
+                .andExpect(xpath("//div[@id='competition-list']/div[1]/div/div/p[1]").string(containsString("Participate and be a part of history, let megacorporations spot you and reserve a spot for you")))
+                .andExpect(xpath("//div[@id='competition-list']/div[1]/div/div/a").string(containsString("Go to competition")))
+                .andExpect(xpath("//div[@id='competition-list']/div[1]/div/div/p[2]/small[1]").string(containsString("2021-01-12 17:55")))
+                .andExpect(xpath("//div[@id='competition-list']/div[1]/div/div/p[2]/small[2]").string(containsString("2021-09-12 18:15")));
+    }
 
+    @Test
+    public void specificCompetitionTest() throws Exception {
+        mockMvc.perform(get("/competition/2"))
+                .andExpect(authenticated())
+                .andExpect(xpath("//div[@id='main']/div/div/img").exists())
+                .andExpect(xpath("//div[@id='main']/div/div/div[1]/h1").string(containsString("Big Data Analysis MegaHackathon Moscow 2021")))
+                .andExpect(xpath("//div[@id='main']/div/div/div[1]/h6").string(containsString("Organized by Егор")))
+                .andExpect(xpath("//div[@id='main']/div/div/div[1]/p[1]").string(containsString("Participate and be a part of history, let megacorporations spot you and reserve a spot for you")))
+                .andExpect(xpath("//div[@id='main']/div/div/div[1]/p[2]").string(containsString("2021-01-12 17:55")))
+                .andExpect(xpath("//div[@id='main']/div/div/div[1]/p[3]").string(containsString("2021-09-12 18:15")));
+    }
 }
