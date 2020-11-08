23ac152756ec62b64b9cc1525ec1b91651b9298b
plentybugs <plentybugs@yandex.ru>
2020-11-09 00:41:34 +0300

    Добавил возможность листать месяцы

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -22,40 +22,60 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let date = (new Date());
 
-let dateToday = date || new Date(),
-    month = date.getMonth(),
-    year = date.getFullYear(),
-    first = new Date(year, month, 1),
-    last = new Date(year, month + 1, 0),
-    startingDay = first.getDay(),
-    thedate = new Date(year, month, 1 - startingDay)
-    today = new Date();
+let dayToday = date.toDateCssClass();
 
-$("#monthYear").append(months[month] + " " + year)
+function draw() {
+    let dateToday = date || new Date(),
+        month = date.getMonth(),
+        year = date.getFullYear(),
+        first = new Date(year, month, 1),
+        last = new Date(year, month + 1, 0),
+        startingDay = first.getDay(),
+        thedate = new Date(year, month, 1 - startingDay),
+        today = new Date();
 
-let weeksHead = $("#weeksHead");
-for (let i = 0; i < 7; i++) {
-    let dayInHead = $("<th class='c-name'>" + days[i] + "</th>");
-    weeksHead.append(dayInHead);
-}
+    let monthYear = $("#monthYear");
+    monthYear.empty();
+    monthYear.append("<i class='fa fa-chevron-circle-left' id='prevMonth'></i> " + months[date.getMonth()] + " " + date.getFullYear() + " <i class='fa fa-chevron-circle-right' id='nextMonth'></i>")
 
-let daysTable = $("#days");
-
-for (let j = 0; j < 5; j++) {
-    let rowOfDays = $("<tr id='rowOfDays'></tr>");
+    let weeksHead = $("#weeksHead");
+    weeksHead.empty();
     for (let i = 0; i < 7; i++) {
-        let dayClass;
-        if (thedate > last) {
-            dayClass = "outside";
-        } else if (thedate >= first) {
-            dayClass = "current";
-        }
-        let day = $(
-            "<td class=\"calendar-day " + dayClass + " " + thedate.toDateCssClass() + " " + (date.toDateCssClass() === thedate.toDateCssClass() ? 'selected':'') + " js-cal-option\" data-date='" + thedate.toISOString() + "'>" +
-            "<div class='date'>" + thedate.getDate() + "</div></td>"
-        );
-        thedate.setDate(thedate.getDate() + 1);
-        rowOfDays.append(day);
+        let dayInHead = $("<th class='c-name'>" + days[i] + "</th>");
+        weeksHead.append(dayInHead);
     }
-    daysTable.append(rowOfDays);
+
+    let daysTable = $("#days");
+    daysTable.empty();
+
+    for (let j = 0; j < 5; j++) {
+        let rowOfDays = $("<tr id='rowOfDays'></tr>");
+        for (let i = 0; i < 7; i++) {
+            let dayClass;
+            if (thedate > last) {
+                dayClass = "outside";
+            } else if (thedate >= first) {
+                dayClass = "current";
+            }
+            let day = $(
+                "<td class=\"calendar-day " + dayClass + " " + thedate.toDateCssClass() + " " + (dayToday === thedate.toDateCssClass() ? 'selected':'') + " js-cal-option\" data-date='" + thedate.toISOString() + "'>" +
+                "<div class='date'>" + thedate.getDate() + "</div></td>"
+            );
+            thedate.setDate(thedate.getDate() + 1);
+            rowOfDays.append(day);
+        }
+        daysTable.append(rowOfDays);
+    }
+    daysTable.append($("<tr id='rowOfDays'><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>"));
+
+    $("#prevMonth").click(() => {
+        date.setMonth(date.getMonth() - 1);
+        draw();
+    });
+    $("#nextMonth").click(() => {
+        date.setMonth(date.getMonth() + 1);
+        draw();
+    });
 }
+
+draw();
