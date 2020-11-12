cc9cd9fc6802a775ddb06b07fb0590d07b0f6550
plentybugs <plentybugs@yandex.ru>
2020-11-12 21:57:16 +0300

    исправил usr на user в репозитории

--- src/main/java/org/netcracker/project/repository/CompetitionRepository.java
+++ src/main/java/org/netcracker/project/repository/CompetitionRepository.java
@@ -1,5 +1,6 @@
 package org.netcracker.project.repository;
 
+import org.apache.tomcat.jni.Local;
 import org.netcracker.project.model.Competition;
 import org.netcracker.project.model.Team;
 import org.netcracker.project.model.User;
@@ -7,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,6 +22,12 @@ public interface CompetitionRepository extends JpaRepository<Competition,Long> {
     Page<Competition> findAllByStartDateAfter(Pageable pageable, LocalDateTime startDate);
 
     Page<Competition> findAllByStartDateBefore(Pageable pageable, LocalDateTime startDate);
+
+    @Query("from Competition  c where c.endDate < :endDate and (c.organizer = :user)")
+    Page<Competition> getArchiveByUser(Pageable pageable, LocalDateTime endDate, User user);
+
+    @Query("from Competition  c where c.endDate >= :endDate and (c.organizer = :user)")
+    Page<Competition> getRunningCompByUser(Pageable pageable, LocalDateTime endDate,User user);
 
     @Query("from Competition c where c.startDate = :startDate and (lower(c.description) like lower(:search) or lower(c.compName) like lower(:search))")
     Page<Competition> findAllByStartDateEquals(Pageable pageable, LocalDateTime startDate, String search);
@@ -38,21 +44,15 @@ public interface CompetitionRepository extends JpaRepository<Competition,Long> {
     @Query("from Competition c where :team member of c.teams")
     List<Competition> findAllByTeam(Team team);
 
-    @Query("from Competition c where exists (select t.id from c.teams as t where :teamId = t.id) and c.startDate >= :startDate or c.startDate <= :endMonthDate")
+    @Query("from Competition c where exists (select t.id from c.teams as t where :teamId = t.id) and c.startDate >= :startDate and c.startDate <= :endMonthDate")
     List<Competition> findAllByTeamCalendar(Long teamId, LocalDateTime startDate, LocalDateTime endMonthDate);
 
     @Query("from Competition c where exists (select t.id from Team as t where :user member of t.teammates)")
     List<Competition> findAllByUser(User user);
 
-    @Query("from Competition c where exists (select t.id from Team as t where :user member of t.teammates) and c.startDate >= :startDate or c.startDate <= :endMonthDate")
+    @Query("from Competition c where exists (select t.id from Team as t where :user member of t.teammates) and c.startDate >= :startDate and c.startDate <= :endMonthDate")
     List<Competition> findAllByUserCalendar(User user, LocalDateTime startDate, LocalDateTime endMonthDate);
 
-    @Query("from Competition c where c.startDate >= :startDate or c.startDate <= :endMonthDate")
+    @Query("from Competition c where c.startDate >= :startDate and c.startDate <= :endMonthDate")
     List<Competition> findAllCalendar(LocalDateTime startDate, LocalDateTime endMonthDate);
-
-    @Query("from Competition  c where c.endDate < :endDate and (c.organizer = :user)")
-    List<Competition> getArchiveByUser(LocalDateTime endDate, User user);
-
-    @Query("from Competition  c where c.endDate >= :endDate and (c.organizer = :user)")
-    List<Competition> getRunningCompByUser(LocalDateTime endDate,User user);
 }
