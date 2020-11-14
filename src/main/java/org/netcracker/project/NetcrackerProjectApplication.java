38bee06597a021a12bc5979e9ed210fc720c2eb6
plentybugs <plentybugs@yandex.ru>
2020-11-15 02:22:42 +0300

    Добавил стабильный коннект к сокету и отправку сообщений, начал делать фронт часть чатов

--- src/main/java/org/netcracker/project/NetcrackerProjectApplication.java
+++ src/main/java/org/netcracker/project/NetcrackerProjectApplication.java
@@ -2,10 +2,11 @@ package org.netcracker.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { EmbeddedMongoAutoConfiguration.class })
 @EnableAsync
 @EnableSpringDataWebSupport
 public class NetcrackerProjectApplication {
--- src/main/resources/static/js/messenger.js
+++ src/main/resources/static/js/messenger.js
@@ -1,11 +1,12 @@
 let stompClient = null;
+let token = $('#_csrf').attr('content');
+let header = $('#_csrf_header').attr('content');
+let userId = $("#zzz").attr("value");
 
 function connect() {
     let socket = new SockJS("/ws");
-    let userId = $("#zzz").attr("value");
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        console.log("connected");
         stompClient.subscribe("/user/" + userId + "/queue/messages", function (message) {
             console.log
             (message);
@@ -19,3 +20,37 @@ function disconnect() {
     }
     console.log("disconnected")
 }
+
+const sendMessage = (msg, senderId, recipientId, senderName, recipientName) => {
+    if (msg.trim() !== "") {
+        const message = {
+            senderId: senderId,
+            recipientId: recipientId,
+            senderName: senderName,
+            recipientName: recipientName,
+            content: msg,
+            time: new Date()
+        };
+
+        stompClient.send("/app/chat", {}, JSON.stringify(message));
+    }
+};
+
+function showChat(recipientId) {
+    let chat = {};
+    const regexp = /(.+?)messenger(.*)/;
+    const url = document.URL.match(regexp)[1] + "messenger/" + senderId + "/chat/" + recipientId;
+    $.ajax(
+        {
+            type: 'GET',
+            url: url,
+            beforeSend: (xhr) => xhr.setRequestHeader(header, token),
+            success: (data) => chat = data,
+            cache: false,
+            async: false
+        }
+    )
+    console.log(chat);
+}
+
+$(() => connect());
