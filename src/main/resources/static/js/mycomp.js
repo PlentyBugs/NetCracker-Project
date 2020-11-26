1be750dc21f7e6151f598cb3bd2659613b3b091e
RollerM0bster <katerinasheludenko@gmail.com>
2020-11-26 04:53:26 +0300

    Отредактировала mycomp.js.

--- src/main/resources/static/js/mycomp.js
+++ src/main/resources/static/js/mycomp.js
@@ -18,37 +18,40 @@ function getCompetitions() {
         async: false
     });
 
-    var archList=getList(archiveComp);
-    let runningList=getList(runningComp);
+    var archList = getList(archiveComp);
+    let runningList = getList(runningComp);
     let archive = $("#archive");
     let running = $("#running");
     let compArchive = $(
-        "<div class='text-center'><ul class='list-group text-center'>" + archList + "</ul></div>" +
+        "<div><ul class='list-group'>" + archList + "</ul></div>" +
         "</div>" +
         "        </div>");
     let compRun = $(
-        "<div class='text-center'><ul class='list-group text-center'>" + runningList + "</ul></div>" +
+        "<div><ul class='list-group'>" + runningList + "</ul></div>" +
         "</div>" +
         "        </div>");
     archive.append(compArchive);
     running.append(compRun);
 }
 
-function getList(array){
-    let name=[];
-    let desc=[];
-    var result=[];
-    for(let i=0;i<array.length;i++){
-        $.each(array[i],function (key,value){
-            if (key=="compName"){
-                name[i]=value;
-            }
-            if(key=="description"){
-                desc[i]=value;
-            }
+function getList(array) {
+    let name = [];
+    let desc = [];
+    let titleFile = [];
+    var result = [];
+    for (let i = 0; i < array.length; i++) {
+        $.each(array[i], function (key, value) {
+            if (key == "compName") name[i] = value;
+            if (key == "description") desc[i] = value;
+            if (key == "titleFilename") titleFile[i] = value;
         });
-        result+='<div class="col-sm-8 align-self-center"><li class="list-group-item bg-dark">'+name[i]+'<br>'+desc[i]+'</li></div>';
+        result += "<div class='container'>" +
+            "<div class='card-body bg-dark'>" +
+            "<h4 class='card-title'>" + name[i] + "</h4>" +
+            "<p class='card-text'>" + desc[i] + "</p>" +
+            "</div></div>";
     }
     return result;
 }
+
 $(() => getCompetitions())
